f8c13a2d44c7d33f64750c6a6325b30c0fe2b4b1
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-02-16 12:45:11 +0500

    Squid Game

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let menuToggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+let button = document.querySelector('.btn');
+let cart = document.querySelector('.ticket-cart');
+button.addEventListener('click', () => {
+    cart.classList.toggle('animateMe');
+})
+let cartClose = document.querySelector('.close-Cart');
+cartClose.addEventListener('click', () => {
+    cart.classList.toggle('animateMe');
+})
